7195a64cf74343521f7db8a0d30124f2f77c6293
GregChan <gregorywarrenchan@gmail.com>
2013-11-25 02:50:37 -0600

    Greg Chan: pdassiCrawler.py

---
+++ pdassiCrawler.py
@@ -0,0 +1,34 @@
+import urllib2
+
+def getDownloadURL(appStoreURL):
+	try:
+		response = urllib2.urlopen(appStoreURL)
+		html = response.read()
+		index = html.index('http://pdassi.de/util/')
+		downloadURL = html[index:].split('\"')[0]
+		print downloadURL
+		return downloadURL
+	except urllib2.HTTPError:
+		print "Error URL not found"
+		return 0
+
+def searchURLs():
+	listOfURLs = [] 
+	for i in range(0, 999999):
+		url = 'http://android.pdassi.de/' + str(i) + '/'
+		downloadURL = getDownloadURL(url).replace(' ', '%20').replace('amp;', '')
+		if(downloadURL):
+			fileName = downloadURL[downloadURL.index('?')+1:].split('.apk')[0]
+			listOfURLs.append(downloadURL)
+			downloadApp(downloadURL, fileName)
+
+	print listOfURLs
+
+def downloadApp(url, fileName):
+	file = urllib2.urlopen(url)
+	output = open(fileName + '.apk', 'wb')
+	output.write(file.read())
+	output.close()
+
+
+searchURLs()
