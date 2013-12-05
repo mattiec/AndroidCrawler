43e4c8345d3db5473391356a77990354b4557755
GregChan <gregorywarrenchan@gmail.com>
2013-12-05 04:37:07 -0600

    added a try catch to continue downloads with bad app names

--- blapkmarketCrawler.py
+++ blapkmarketCrawler.py
@@ -50,12 +50,15 @@ for page in range(1, 12):
 		
 		outputString = outputString + appName + "\n"
 		
-		#download the application	
-		fileRequest = urllib2.Request(downloadURL, data)
-		file = urllib2.urlopen(fileRequest)			
-		output = open(path + appName + ".apk", "wb")
-		output.write(file.read())
-		output.close()
+		try:		
+			#download the application	
+			fileRequest = urllib2.Request(downloadURL, data)
+			file = urllib2.urlopen(fileRequest)			
+			output = open(path + appName + ".apk", "wb")
+			output.write(file.read())
+			output.close()
+		except:
+			print "Warning the file name was no good and so this application was skipped it."
 
 output = open("listOfApps.txt", "wb")
 output.write(outputString)
